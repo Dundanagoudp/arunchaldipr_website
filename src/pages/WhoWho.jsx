475991bbc664f0c4e47cc9d831f4acd3a73cbc54
chetanv18~ <chetan@gully2global.com>
2024-11-14 15:30:44 +0530

    new

--- src/pages/WhoWho.jsx
+++ src/pages/WhoWho.jsx
@@ -1,11 +1,186 @@
 import "../pages/WhoWho.css";
 
-export const Whowho=()=>{
-      return(
-            <div className="whowho">
-                  <nav className="breadcrumb">
-        <a href="/">Home</a> » <a href="#">who's who</a>
+export const Whowho = () => {
+  return (
+    <div className="whowho">
+      <nav className="breadcrumb">
+        <a href="/">Home</a> » <a href="#">Who's Who</a>
       </nav>
-            </div>
-      );
+
+      <h4>
+        Employment List of Arunachal Government Officials (As of 30/08/2024)
+      </h4>
+
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Sl.No</th>
+              <th>Name of the Officer</th>
+              <th>Designation/Departments</th>
+              <th>Block/Room Details</th>
+              <th>Contact Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Shri Manish Gupta, IAS (AGMUT:91)</td>
+              <td>Chief Secretary</td>
+              <td>Block-II 5th Floor</td>
+              <td>2212444 (Res), cs-arunachal@nic.in</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Shri Kaling Tayeng, IAS (AGMU:98)</td>
+              <td>
+                Principal Secretary: Political, PWD, Home, Border Affairs,
+                Election
+              </td>
+              <td>Block-II 3rd Floor Room No. 316</td>
+              <td>2324 (PBX), pwdsecyap@gmail.com</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Smt. Padmini Singla, IAS (AGMUT:2002)</td>
+              <td>Chairman APSSB</td>
+              <td>APSSB, PSC Building, Vidhan Vihar, Itanagar, A.P.</td>
+              <td>9485231286 (Office)</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Sri Ankur Garg, IAS (AGMUT:2003)</td>
+              <td>Commissioner: Economics & Statistics, Coordination</td>
+              <td>Block-II 3rd Floor Room No. 302</td>
+              <td>2212396 (off), gargankur1980@gmail.com</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Shri Sonam Chombay, IRS (AGMUT:2003)</td>
+              <td>
+                1. Commissioner to HCM
+                <br />
+                2. Joint Secy (Cabinate)
+              </td>
+              <td>Block-II 6th Floor</td>
+              <td>
+                PBX-2630
+                <br />
+                sonamchambey@gmail.com
+              </td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>Shri Vivek Pandey, IAS (AGMUT: 2003)</td>
+              <td>
+                Commissioner
+                <br />
+                1. Transport
+                <br />
+                2. Agriculture Production Commissioner
+                <br />
+                3. Urban Affairs (Urban Development, Town Planning, Urban Local
+                Bodies, Housing)
+                <br />
+                Divisional Commissioner East Zone
+              </td>
+              <td>Block-II 3rd Floor Room No. 321</td>
+              <td>
+                PBX-2342
+                <br />
+                2217852
+                <br />
+                commissionertpt24@gmail.com
+                <br />
+                Vivekias.2003@gmail.com
+              </td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>Sri Amjad Tak, IAS (AGMUT:2005)</td>
+              <td>
+                Commissioner
+                <br />
+                1. Education
+                <br />
+                2. Food & Civil Supplies
+                <br />
+                3. Consumer Affairs
+                <br />
+                Divisional Commissioner (Central Zone)
+              </td>
+              <td>Block-II- 2nd Floor, Room No. 202</td>
+              <td>
+                2290253 (off)
+                <br />
+                2215810 (Res)
+                <br />
+                PBX - 2220
+                <br />
+                secretaryeducation@gmail.com
+              </td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>Shri Pawan Kumar Sain, IAS (AGMUT:2005)</td>
+              <td>
+                Commissioner
+                <br />
+                1. Health & Family Welfare
+                <br />
+                2. Public Health Engineering & Water Supply / Rural Sanitation &
+                Water Supply
+                <br />
+                CEO, Arunachal Pradesh
+              </td>
+              <td>Block-II- 3rd Floor, Room No. 320</td>
+              <td>
+                Ceo-arunachalpradesh@eci.gov.in
+                <br />
+                0360-2212437
+              </td>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>Shri Saugat Biswas IAS (AGMUT:2006)</td>
+              <td>
+                Commissioner
+                <br />
+                1. Industries
+                <br />
+                2. Trade & Commerce
+                <br />
+                3. Skill Development & Entrepreneurship
+                <br />
+                4. Labour and Employment
+              </td>
+              <td>Block-II- First Floor Room No. 122</td>
+              <td>
+                2211236
+                <br />
+                saughat.biswas@nic.in
+              </td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>Shri Bidol Tayeng IAS (AGMUT:2007)</td>
+              <td>
+                Commissioner
+                <br />
+                Agriculture
+              </td>
+              <td>Block-2 2nd Floor Room No. 216</td>
+              <td>
+                PBX-1302
+                <br />
+                bidoltayeng250@gmail.com
+                <br />
+                secyagriarun@gmail.com
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
