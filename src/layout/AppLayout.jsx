35b358efa5b322c3dccd3cab7ac6cb35ea0bf880
chetanv18~ <chetan@gully2global.com>
2024-11-15 11:04:37 +0530

    new

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,21 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../navbar/Header";
 import { Footer } from "../Footer/Footer";
-// import { LanguageProvider } from '../context/LanguageContext';
-
+import { LanguageProvider } from '../context/LanguageContext';
 
 const AppLayout = () => {
-  
   return (
-    <>
-        {/* <LanguageProvider> */}
-
-      <Header  /> 
-      <Outlet  /> 
+    <LanguageProvider>
+      <Header />
+      <Outlet />
       <Footer />
-      {/* </LanguageProvider> */}
-
-    </>
+    </LanguageProvider>
   );
 };
 
